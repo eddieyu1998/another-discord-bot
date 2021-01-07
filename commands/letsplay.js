767ff6f374df053db287337e2a860d20743da7b3
eddieyu <eddieyu1998@gmail.com>
2021-01-07 15:32:39 +0000

    Add parser to parse value of the options

--- commands/letsplay.js
+++ commands/letsplay.js
@@ -1,3 +1,5 @@
+const { Message } = require("discord.js")
+const { parse } = require("yargs")
 const { allowExecute } = require("../common")
 const guildOnly = true
 
@@ -35,10 +37,107 @@ exports.handler = function (argv) {
 
 function execute(argv) {
     console.log(argv)
+    try {
+        const game = argv.game ? parseGame(argv.game) : null
+        const player = argv.player ? parsePlayer(argv.player) : null
+        const at = argv.at ? parseAt(argv.at) : null
+    } catch (err) {
+        argv.message.reply(err.message)
+    }
+}
+
+function parseGame(gameOption) {
+    return gameOption
+}
+
+/**
+ * @typedef {Object} Player
+ * @property {number} exact
+ * @property {number} min
+ * @property {number} max
+ */
+/**
+ * A function to parse the player option
+ *
+ * @param {string} playerOption - player option in the command
+ * @returns {Player}
+ *
+ * player option can take the following 3 forms:
+ * 4
+ * 4+
+ * 4-6
+ */
+function parsePlayer(playerOption) {
+    const re = /^([1-9]\d*)(\+|-([1-9]\d*))?$/
+    const match = playerOption.match(re)
+    if (!match) {
+        throw new Error("Unsupported format for option [player]")
+    }
+    // for playerOption "1-9" match return [ '1-9', '1', '-9', '9', index: 0, input: '1-9', groups: undefined ]
+    const player = {}
+    if (match[3]) {
+        // e.g. 1-9
+        if (parseInt(match[3]) <= parseInt(match[1])) {
+            throw new Error("Invalid player number")
+        }
+        player.min = parseInt(match[1])
+        player.max = parseInt(match[3])
+    } else if (match[2]) {
+        // e.g. 2+
+        player.min = parseInt(match[1])
+    } else {
+        // e.g. 4
+        player.exact = parseInt(match[1])
+    }
+    return player
+}
+
+/**
+ * @typedef {Object} At
+ * @property {string} display
+ * @property {Date} deadline
+ */
+/**
+ * A function to parse the at option
+ * Currently only support time in the next 24 hours
+ *
+ * @param {string} atOption
+ * @returns {At}
+ * supported format (in 24-hour):
+ * H:mm / HH:mm / HHmm / Hmm / HH / H
+ */
+function parseAt(atOption) {
+    const re = /^(\d{1,2}):?(\d{2})?$/
+    const match = atOption.match(re)
+    if (!match) {
+        throw new Error("Unsupported format for option [at]")
+    }
+    let hour = null,
+        minute = null
+    hour = match[1]
+    if (match[2]) {
+        minute = match[2]
+    }
+    if (parseInt(hour) >= 24 || (minute && parseInt(minute) >= 60)) {
+        throw new Error("Invalid time value")
+    }
+    const at = {}
+    at.display = hour.padStart(2, "0") + ":" + (minute ? minute : "00")
+    let deadline = new Date()
+    deadline.setHours(hour)
+    deadline.setMinutes(minute)
+    if (deadline < new Date()) {
+        deadline.setDate(deadline.getDate() + 1)
+    }
+    at.deadline = deadline
+    return at
 }
 
 /*
 command examples
+
+looking to play any game with any number of people
+/letsplay any
 
 looking to play Minecraft with any number of people
 /letsplay Minecraft
