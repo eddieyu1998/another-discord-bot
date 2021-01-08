fc156569654917fd18d50b01cf588fe3c3a3d1b8
eddieyu <eddieyu1998@gmail.com>
2021-01-08 10:11:04 +0000

    Add reaction collector and game start message

--- commands/letsplay.js
+++ commands/letsplay.js
@@ -1,9 +1,10 @@
 const fs = require("fs")
-const { parse } = require("path")
 const util = require("util")
 const { allowExecute } = require("../common")
 const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
+
+const defaultExpire = 86400000 // default 24 hours expiration
 
 const guildOnly = true
 
@@ -43,12 +44,7 @@ exports.handler = function (argv) {
 }
 
 async function execute(argv) {
-    console.log(argv)
     try {
-        const gameName = argv.game ? parseGame(argv.game) : null
-        const player = argv.player ? parsePlayer(argv.player) : null
-        const at = argv.at ? parseAt(argv.at) : null
-
         const lobby = {}
         if (argv.game) lobby.game = parseGame(argv.game)
         if (argv.player) lobby.player = parsePlayer(argv.player)
@@ -60,7 +56,9 @@ async function execute(argv) {
         const lobbyMessage = await argv.message.channel.send(newLobbyMessage(argv.message, lobby))
         lobbyMessage.react("✅")
 
-        // await reaction
+        // TODO: create scheduled reminder?
+        const timeRemain = lobby.at ? lobby.at.deadline - Date.now() : null
+        collectReaction(lobbyMessage, timeRemain, lobby.player)
 
         const guildId = argv.message.guild.id
         lobby.id = lobbyMessage.id
@@ -76,6 +74,81 @@ function newLobbyMessage(message, lobby) {
     const deadline = lobby.at ? `at **${lobby.at.display}**` : ""
     const response = `${message.author} wants to play ${game} with ${player} players ${deadline}. React below to join. @everyone`
     return response
+}
+
+function isPlayerEnough(player, current) {
+    if (player.exact) {
+        return current === player.exact
+    }
+    if (player.min) {
+        return current >= player.min
+    }
+    return false
+}
+
+function collectReaction(message, timeRemain, player) {
+    const filter = (reaction, user) => {
+        return reaction.emoji.name === "✅"
+    }
+
+    const collectorOption = {}
+    collectorOption.time = timeRemain ? Math.max(timeRemain, 5000) : defaultExpire
+    if (player && player.max) collectionOption.max = player.max
+    const collector = message.createReactionCollector(filter, { time: 3000 })
+
+    collector.on("collect", (reaction, user) => {
+        // check if enough people, and ask if force start
+        if (player && isPlayerEnough(player, reaction.count - 1)) {
+            // TODO: ask for early start (once ask once)
+        }
+    })
+
+    collector.on("end", async (collected) => {
+        // TODO: implement startGame() and isGameStarted()
+        if (false && isGameStarted()) {
+            return
+        }
+
+        // determine if able to start game
+        let canStartGame
+        if (player) {
+            canStartGame = isPlayerEnough(player, collected.get("✅").count - 1)
+        } else {
+            // no player number option supplied, need at least 2
+            canStartGame = collected.get("✅").count - 1 > 1 ? true : false
+        }
+
+        //const message = collected.get("✅").message
+        const participants = Array.from(collected.get("✅").users.cache.values()).filter((user) => !user.bot)
+
+        // switch to message.reply when discord.js v13 is ready
+        if (canStartGame) {
+            // mention all participants to start the game
+            let gameStartMessage = `The game is ready to start.`
+            participants.forEach((user) => (gameStartMessage += ` ${user}`))
+            message.channel.send(gameStartMessage)
+            // change status, participants in the guild file
+            const guildId = message.guild.id
+            const newLobby = { status: "playing", participants: participants.map((user) => user.id) }
+            try {
+                const guildFile = await readFile(`./guilds/${guildId}.json`)
+                const guild = JSON.parse(guildFile)
+                const lobby = guild["lobbies"].find((lobby) => lobby.id === message.id)
+                if (lobby) {
+                    Object.assign(lobby, newLobby)
+                    const data = JSON.stringify(guild)
+                    await writeFile(`./guilds/${guildId}.json`, data)
+                }
+            } catch (err) {
+                // do nothing
+                console.error(err)
+            }
+        } else {
+            let gameExpireMessage = `There is not enough people to start the game.`
+            participants.filter((user) => !user.bot).forEach((user) => (gameExpireMessage += `${user}`))
+            message.channel.send(gameExpireMessage)
+        }
+    })
 }
 
 // A function to add the new lobby to the guild file
